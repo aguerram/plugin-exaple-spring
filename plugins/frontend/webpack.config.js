3ba22a1e9a8b0dd2a26a15942fd7e5e4b1f8d3c9
aguerram <agurram2013@gmail.com>
2021-04-17 00:37:39 +0000

    changes in webpacl

--- plugins/frontend/webpack.config.js
+++ plugins/frontend/webpack.config.js
@@ -1,7 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
+const CompressionPlugin = require("compression-webpack-plugin");
 
 const outPutDir = path.resolve(__dirname, '../src/main/resources/static')
+const isProd = process.env.NODE_ENV === "production"
+const productionPlugins = isProd ?
+    [
+        new CompressionPlugin({
+            algorithm: "gzip",
+        })
+    ] :
+    []
 
 module.exports = {
     entry: path.resolve(__dirname, './src/main.js'),
@@ -22,7 +31,10 @@ module.exports = {
         path: outPutDir,
         filename: 'bundle.js',
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()],
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        ...productionPlugins
+    ],
     devServer: {
         contentBase: outPutDir,
         hot: true
